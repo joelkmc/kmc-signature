7feadf7cd36b6ca919b7b6bdd0ba91077e1338cf
Jhon Joel <cabagua.joel@gmail.com>
2021-08-19 10:47:03 +0800

    Added counter for redirecting user from contract to bookings

---
+++ src/component/module/PageHeading/SuccessfulDialog.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import { useContractStore } from '../../../store/contractStore'
+import DialogComp from '../../element/Dialog'
+
+interface SuccessfulDialogProps {
+  dialogState: boolean
+  closeDialog: () => void
+}
+
+const SuccessfulDialog: React.FC<SuccessfulDialogProps> = ({ closeDialog, dialogState }) => {
+  const [counter, setCounter] = useState(5)
+  const bookingNumber = useContractStore((state) => state.bookingNumber)
+
+  useEffect(() => {
+    !dialogState && setCounter(5)
+
+    setTimeout(() => {
+      if (dialogState && counter > 0) {
+        setCounter((old) => old - 1)
+      }
+    }, 1000)
+
+    if (counter === 0) {
+      window.location.href =
+        process.env.NODE_ENV === 'development'
+          ? `http://localhost:3000/user/bookings/${bookingNumber}`
+          : `https://kmc-hub.vercel.app/user/bookings/${bookingNumber}`
+    }
+  }, [counter, dialogState, bookingNumber])
+
+  return (
+    <DialogComp closeDialog={closeDialog} dialogState={dialogState} title="Success!">
+      <div className="py-5">
+        <p>You&apos;re almost done!</p>
+        <p className="mt-3 text-sm">
+          You have successfully signed your agreement with us! We will redirect you back to your
+          booking so you can complete your registration and payment!
+        </p>
+      </div>
+
+      {counter === 0 ? (
+        <p className="text-center text-gray-400">Redirecting...</p>
+      ) : (
+        <div className="border rounded-full border-blue-400 text-blue-600 w-20 h-20 mx-auto flex justify-center items-center">
+          <span className="text-3xl">{counter}</span>
+        </div>
+      )}
+
+      <div className="text-center mt-7">
+        <p className="text-sm">Dont want to wait?</p>
+        <button
+          className="disabled:opacity-80 inline-flex items-center px-4 text-sm text-blue-500 focus:outline-none focus:underline"
+          onClick={closeDialog}
+        >
+          Click here to redirect.
+        </button>
+      </div>
+    </DialogComp>
+  )
+}
+
+export default SuccessfulDialog
--- src/component/module/PageHeading/index.tsx
+++ src/component/module/PageHeading/index.tsx
@@ -1,11 +1,13 @@
 import useDialogState from '../../../hooks/useDialogState'
 import { useContractStore } from '../../../store/contractStore'
 import SignatureDialog from './SignatureDialog'
+import SuccessfulDialog from './SuccessfulDialog'
 
 const PageHeading: React.FC = () => {
   const [dialogState, closeDialog, openDialog] = useDialogState()
+  const [successfulDialog, closeSuccessfulDialog, openSuccessfulDialog] = useDialogState()
+
   const isSignButtonDisabled = useContractStore((state) => state.isSignButtonDisabled)
-  const [bookingNumber] = useContractStore((state) => [state.bookingNumber])
 
   return (
     <div className="mt-2 flex sm:flex-row justify-center sm:justify-end">
@@ -21,22 +23,12 @@ const PageHeading: React.FC = () => {
           </button>
         )}
 
-        {isSignButtonDisabled && (
-          <button
-            disabled={!isSignButtonDisabled}
-            onClick={() =>
-              (window.location.href =
-                process.env.NODE_ENV === 'development'
-                  ? `http://localhost:3000/user/bookings/${bookingNumber}`
-                  : `https://hub.kmc.solutions/user/bookings/${bookingNumber}`)
-            }
-            type="button"
-            className="sm:ml-3 inline-flex items-center px-4 py-2 rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-60"
-          >
-            Go to Bookings Page
-          </button>
-        )}
-        <SignatureDialog closeDialog={closeDialog} dialogState={dialogState} />
+        <SignatureDialog
+          closeDialog={closeDialog}
+          dialogState={dialogState}
+          openSuccessfulDialog={openSuccessfulDialog}
+        />
+        <SuccessfulDialog closeDialog={closeSuccessfulDialog} dialogState={successfulDialog} />
       </div>
     </div>
   )
