1d11c2d59b39f3d0450b6aa8dc6ae26c11fcaae9
Jhon Joel <cabagua.joel@gmail.com>
2021-10-11 19:16:07 +0800

    REfactor

--- src/component/module/PageHeading/index.tsx
+++ src/component/module/PageHeading/index.tsx
@@ -7,12 +7,19 @@ const PageHeading: React.FC = () => {
   const [dialogState, closeDialog, openDialog] = useDialogState()
   const [successfulDialog, closeSuccessfulDialog, openSuccessfulDialog] = useDialogState()
 
-  const isSignButtonDisabled = useContractStore((state) => state.isSignButtonDisabled)
+  const [isSignButtonDisabled, bookingNumber] = useContractStore((state) => [
+    state.isSignButtonDisabled,
+    state.bookingNumber,
+  ])
+
+  const handleGoToBooking = (): void => {
+    window.location.href = `https://kmc-hub.vercel.app/user/bookings/${bookingNumber}`
+  }
 
   return (
     <div className="mt-2 flex sm:flex-row justify-center sm:justify-end">
       <div className="my-2 flex-shrink-0 flex md:mt-0 md:ml-4 space-x-3">
-        {!isSignButtonDisabled && (
+        {!isSignButtonDisabled ? (
           <button
             onClick={openDialog}
             disabled={isSignButtonDisabled}
@@ -20,6 +27,14 @@ const PageHeading: React.FC = () => {
             className="sm:ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-60"
           >
             Click to sign
+          </button>
+        ) : (
+          <button
+            onClick={handleGoToBooking}
+            type="button"
+            className="sm:ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-60"
+          >
+            View Booking Details
           </button>
         )}
 
